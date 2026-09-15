52efba848cba9dbb7e5f5b84cdb8e70aa97bef84
Stanley Kwok (JOS Onsite Support) <sakwok@hktdc.org>
2026-09-15 17:54:45 +0800

    fix: export schema object from schemaTypes

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,3 +1,9 @@
+import { type SchemaTypeDefinition } from 'sanity'
 import { bookType } from './book'
 
-export const schemaTypes = [bookType]
+export const schemaTypes: SchemaTypeDefinition[] = [bookType]
+
+// 新增此匯出以適應 sanity.config.ts 的預設要求
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [bookType],
+}
